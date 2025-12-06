83fdcc15c84726fce247725eef7b7de916230c19
san928811-pixel <san928811@gmail.com>
2025-12-06 14:34:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,31 @@
 from telegram.ext import Updater, CommandHandler
 
+# --- Your Bot Token ---
 TOKEN = "7936792037:AAEY8w1SamKAanqZr66Lbfd_DKUK0GUzC18"
 
+# /start command
 def start(update, context):
-    update.message.reply_text("Bot chal raha hai! 🔥")
+    update.message.reply_text("Bot chal raha hai! 👋")
 
+# /broadcast command
 def broadcast(update, context):
     msg = " ".join(context.args)
     if not msg:
-        update.message.reply_text("Kya bhejunga? Message likho.")
+        update.message.reply_text("Kya message bhejna hai?")
         return
 
-    users = [7895892794]  # Apni Telegram ID
+    # Sirf un logon ko message bhejega jinko aap list me add karoge
+    users = [7895892794]   # <-- Apna Telegram User ID
+
     for u in users:
         try:
             context.bot.send_message(chat_id=u, text=msg)
         except:
             pass
 
-    update.message.reply_text("Message sabko bhej diya! ✔")
+    update.message.reply_text("Message successfully broadcast ho gaya! ✅")
 
+# --- Run Application ---
 updater = Updater(TOKEN, use_context=True)
 dp = updater.dispatcher
 
