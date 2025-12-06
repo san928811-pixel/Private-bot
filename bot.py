5fe688053d1a0f695210502561e44ada8a900640
san928811-pixel <san928811@gmail.com>
2025-12-06 17:40:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,22 @@
 from telegram.ext import Updater, CommandHandler
+# ---- YOUR BOT TOKEN ----
+TOKEN = "7936792037:AAEY8w1SamKAanqZr66Lbfd_DKUK0GUzC18"
 
-# YOUR BOT TOKEN
-TOKEN = "xxxxxxxx"
-
+# ---- /start command ----
 def start(update, context):
     update.message.reply_text("Bot सफलतापूर्वक चल रहा है ✔️")
 
+# ---- /broadcast command ----
 def broadcast(update, context):
     msg = " ".join(context.args)
+
     if not msg:
-        update.message.reply_text("❗Broadcast भेजने के लिए /broadcast text")
+        update.message.reply_text("❗ Broadcast भेजने के लिए: /broadcast आपका_मैसेज")
         return
 
-    users = [7895892794]
+    # जिन users को message भेजना है उनकी list
+    users = [7895892794]   # यहाँ अपनी Telegram ID डालो
+
     for u in users:
         try:
             context.bot.send_message(chat_id=u, text=msg)
@@ -21,6 +25,7 @@ def broadcast(update, context):
 
     update.message.reply_text("✔️ Message भेज दिया गया")
 
+# ---- RUN BOT ----
 updater = Updater(TOKEN, use_context=True)
 dp = updater.dispatcher
 
